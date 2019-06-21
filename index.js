55e9ea8953d069a3ce1ce69aae87b3052102a142
Your Name <aujones95@gmail.com>
2019-06-21 13:11:29 -0400

    still working

--- index.js
+++ index.js
@@ -40,6 +40,52 @@ server.get('/api/projects/:id', async(req, res) => {
     }
 })
 
+server.post('/api/projects', async(req, res) => {
+    try{
+        const [id] = await db('projects').insert(req.body);
+        const project = await db('projects')
+        .where({ id })
+        .first();
+        res.status(201).json(project)
+    }catch (error) {
+        const message = errors[error.errno] || 'there is an error'
+        res.status(500).json({ message, error})
+    }
+})
+
+server.put('/api/projects/:id', async(req, res) => {
+    try {
+        const count = await db('projects')
+        .where({ id: req.params.id })
+        .update(req.body)
+        if (count > 0) {
+            const project = await db('projects')
+            .where({ id: req.params.id })
+            .first();
+            res.status(200).json(project)
+        }else{
+            res.status(404).json({ message: 'Not Found'})
+        }
+    } catch (error) {}
+})
+
+server.delete('/api/projects/:id', async(req, res) => {
+    try{
+        const count = await db('projects')
+        .where({ id: req.params.id })
+        .del();
+        res.status(200).json({ message: 'Deleted'})
+        if (count > 0) {
+            res.status(204).end();
+        }else{
+            res.status(404).json ({message: 'Not Found'})
+        }
+    }catch (error) {
+        res.status(500).json({ message: 'not able to complete'})
+    }
+})
+
+
 
 const port = process.env.PORT || 5500;
 server.listen(port, () => console.log(`\nRunning on port ${port}\n`))
